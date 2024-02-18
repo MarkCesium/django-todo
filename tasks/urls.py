f556fd0c1fd2e53e82235a66cc30af415fd77b81
MarkCesium <cesiummark01@gmail.com>
2024-02-18 21:24:11 +0300

    Add updating tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, update_task
 
 
-urlpatterns = [path("", index)]
+urlpatterns = [
+    path("", index, name="index"),
+    path("update_task/<int:pk>", update_task, name="update_task"),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -10,7 +10,21 @@ def index(request: HttpRequest):
         if data_form.is_valid():
             data_form.save()
         return redirect("/")
+
     tasks: list[Task] = Task.objects.all()
     form = TaskForm()
     context = {"tasks": tasks, "form": form}
     return render(request, "tasks/list.html", context)
+
+
+def update_task(request: HttpRequest, pk: int):
+    task = Task.objects.get(id=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    form = TaskForm(instance=task)
+    context = {"form": form, "id": pk}
+    return render(request, "tasks/update_task.html", context)
