5aa5acea64bcbb2f819446642951f4502b55b1f3
MarkCesium <cesiummark01@gmail.com>
2024-02-18 21:00:58 +0300

    Add and reg model, add form, POST method

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from .models import Task
+from .forms import TaskForm
 
 
 def index(request: HttpRequest):
-    return render(request, "tasks/list.html")
+    if request.method == "POST":
+        data_form = TaskForm(request.POST)
+        if data_form.is_valid():
+            data_form.save()
+        return redirect("/")
+    tasks: list[Task] = Task.objects.all()
+    form = TaskForm()
+    context = {"tasks": tasks, "form": form}
+    return render(request, "tasks/list.html", context)
